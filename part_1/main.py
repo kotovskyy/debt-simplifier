c9c2797572e32aa6fb71dae437ade3c1de02f6fa
acodered <alexkondratru@gmail.com>
2024-04-05 22:41:40 +0200

    Add exception raise when file doesn't exist

--- part_1/main.py
+++ part_1/main.py
@@ -1,14 +1,22 @@
+import os
 import csv
 from typing import List
 
+def _fileExists(filepath: str) -> bool:
+    return os.path.isfile(filepath)
+
 def readData(filepath: str) -> List[List[str]]:
+    if not _fileExists(filepath):
+        raise FileNotFoundError(f"File '{filepath}' does not exist.")
+    
     with open(filepath, newline='') as csvfile:
         data = csv.reader(csvfile)
         data = list(data)
+    
     return data
 
 def main():
-    filepath = "test_data/debts_1.csv"
+    filepath = "test_data/debts.csv"
     readData(filepath)
 
 if __name__ == "__main__":
